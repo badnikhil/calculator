4bbc4b0a3d80eacb292f8b613d374ac82176063f
Nikhil <nikhilljatt@gmail.com>
2025-12-02 00:29:40 +0530

    added stoi and itos but need to make it work

--- calc.asm
+++ calc.asm
@@ -1,15 +1,21 @@
 section .data
-	msg_for_operator db "Enter Operator (+ , - , * or /) : " 
+	msg_for_operator db "Enter Operator (+ - * or /) : " 
 	msg_for_operator_len equ $ - msg_for_operator
 	msg_for_integer_one db "Enter 1st Integer: " 
 	msg_for_integer_one_len equ $ - msg_for_integer_one
 	msg_for_integer_two  db "Enter 2nd Integer: "
 	msg_for_integer_two_len equ $ - msg_for_integer_two
+
 section .bss
-	operator_buffer resb 2 ;input_buf -> label , resb -> reserver bytes , 50 -> reserve 50 bytes
-	integer_one_buffer resb 2
-	integer_two_buffer resb 2
-	result_buffer resb 2
+	operator_buffer resb 2
+	integer_one_buffer resb 10
+	integer_two_buffer resb 10
+	int_1 resq 1
+	int2 resq 1
+	result resq 1
+	result_buffer resb 10
+	result_len_buffer resq 1
+
 section .text
 	global _start
 
@@ -21,19 +27,21 @@ _start:
 	;input operator
 	mov rsi , operator_buffer
 	mov rdx , 2
-	call read
+	call read	
+
 	;print the message for Integer 1
 	mov rsi , msg_for_integer_one
 	mov rdx , msg_for_integer_one_len
 	call print
 	;input 1st integer
 	mov rsi , integer_one_buffer
-	mov rdx , 2
+	mov rdx , 10
 	call read
 	;convert it into Integer
-	mov bl , byte [integer_one_buffer]
-	sub bl , '0'
-	mov byte[integer_one_buffer] , bl
+	mov rsi , integer_one_buffer
+	mov rdx , rax
+	call convert_string_into_integer
+	mov [int_1] , rax 
 	
 	;print the message for Integer 2
 	mov rsi , msg_for_integer_two
@@ -41,60 +49,104 @@ _start:
 	call print
 	;input 2nd integer
 	mov rsi , integer_two_buffer
-	mov rdx , 2
+	mov rdx , 10
 	call read
 	;convert it into Integer
-	mov bl , byte [integer_two_buffer]
-	sub bl , '0'
-	mov byte[integer_two_buffer] , bl
-
+	mov rsi , integer_two_buffer
+	mov rdx , rax 
+	call convert_string_into_integer
+	mov [int2], rax
 	;check what is the operator and call the corresponding subroutine
 	mov al, byte [operator_buffer]
 	cmp al , '+'
 	je addition
-	cmp al , '*'
-	je multiply
-	cmp al , '/'
-	je divide
-	cmp al , '-'
-	je subtract
+
+	;convert result into string
+	mov rsi , result
+	mov rdi , result_buffer
+	mov r10 , result_len_buffer
+	call convert_integer_to_string
+	mov rsi , result_buffer
+	mov rdx , [r10]
+	call print
+
 	call exit
 
 
 
 
+; the address of string to be converted should be in register ->rsi 
+; the length of string should be in register -> rdx
 
+; Uses register -> rcx as pointer of bytes , rax to return 
+convert_string_into_integer:
+	;initialise registers
+	dec rdx
+	xor rcx , rcx
+	xor rax , rax
+	;loop starts
+	.loop:
+	;compare
+	cmp rcx , rdx
+	jge .return
+
+	movzx rbx , byte [rsi + rcx]
+	sub rbx , '0'
+	imul rax , rax , 10
+	add rax , rbx
+	inc rcx 
+	jmp .loop
+	.return:
+	ret
+
+
+; the address of integer to be converted should be in register -> rsi 
+; the address of the string where it will be stored should be in register -> rdi
+; adresss to store length of result string should be in register -> r10
+; Uses register -> rcx as pointer of bytes 
+convert_integer_to_string:
+	;initialize register
+	xor rcx , rcx
+	mov rax , [rsi]
+	mov qword [r10] , 0
+	;loop starts
+	.loop:
+	inc qword [r10]
+	xor rdx , rdx
+	mov rbx , 10 
+	call divide
+	add rdx , '0'
+	mov byte [rdi + rcx] , dl
+	inc rcx
+	cmp rax , 0
+	jne .loop
+	ret
 
 addition:
-	mov al , byte [integer_one_buffer]
-	mov bl , byte [integer_two_buffer] 
-	add al , bl
-	add al ,'0'
-	mov byte [result_buffer] , al
-	mov byte [result_buffer + 1] , 10
-	mov rsi , result_buffer
-	mov rdx , 2
-	call print
-	call exit
+	mov rax , [int_1]
+	add rax , [int2]
+	mov [result] , rax
+	ret
 
 subtract:
-	mov al , byte [integer_one_buffer]
-	mov bl , byte [integer_two_buffer] 
-	sub al , bl
-	add al ,'0'
-	mov byte [result_buffer] , al
-	mov byte [result_buffer + 1] , 10
-	mov rsi , result_buffer
-	mov rdx , 2
-	call print
-	call exit
+	mov rax , [int_1]
+	sub rax , [int2]
+	mov [result] , rax
+	ret
+
+
 
 multiply:
-;Not Implemented Yet
+call exit
 
-
+;higher part of dividend should be in register -> rdx
+;lower part of dividend should be in register -> rax
+;the divisor should be in register -> rbx
+;the return values are in registers -> rax (the quotient) and register -> rdx (Remainder)
 divide:
-;Not implemented Yet
+xor rdx , rdx
+div rbx
+ret
 
 
 ; whatever needs to be printed should be inside register ->rsi , 
@@ -117,7 +169,10 @@ read:
 	mov rdi , 0
 	syscall
 	ret
+
+;you can directly call the exit command 
 exit:
 	mov rax , 60
 	xor rdi , rdi
 	syscall
+
